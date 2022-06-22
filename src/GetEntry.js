e158aa0d9f922bcee1167ff24c599f769e767dcb
rai-arc <raiarcebido@gmail.com>
2022-06-22 15:02:13 -0400

    removal of unnecessary files

---
+++ src/GetEntry.js
@@ -0,0 +1,28 @@
+const axios = require("axios");
+const baseUrl = "https://pokeapi.co/api/v2/pokemon-species/";
+
+/*
+Gets pokemon data based on pokeName
+Filters flavor text data to only English
+Returns one random flavor text entry
+*/
+
+async function GetEntry(pokeName) {
+    if (typeof pokeName == "number") pokeName.toString();
+    if (typeof pokeName == "string") pokeName = pokeName.toLowerCase();
+  
+    try {const {data} = await axios.get(`${baseUrl}/${pokeName}`)
+    const allFlavor = data.flavor_text_entries;
+    const englishList = allFlavor.filter((entry) => entry.language.name === "en");
+    const randomEntry = englishList[Math.floor(Math.random() * englishList.length)];
+    const result = randomEntry.flavor_text;
+    console.log(result);
+    return result; }
+      catch (error) {
+          const message = ("Please use a valid Pokemon name or National Pokedex number!")
+          console.log(message)
+          return message;
+      }
+  }
+
+export default GetEntry
---
+++ src/GetRandomEntry.js
@@ -0,0 +1,30 @@
+const axios = require("axios");
+const baseUrl = "https://pokeapi.co/api/v2/pokemon-species/";
+
+/*
+Gets one random pokemon's data
+Filters flavor text data to only English
+Returns one random flavor text entry
+*/
+
+async function GetRandomEntry(cap = 898) {
+    if (cap < 1 || cap > 898 || isNaN(cap)) {
+      console.log("Please choose a number from 1 to 898 or leave blank!");
+      return "Please choose a number from 1 to 898 or leave blank!";
+    } else {
+      try {const {data} = await axios.get(`${baseUrl}/${Math.floor(Math.random() * cap + 1)}`)
+          const allFlavor = data.flavor_text_entries;
+          const englishList = allFlavor.filter((entry) => entry.language.name === "en");
+          const randomEntry = englishList[Math.floor(Math.random() * englishList.length)];
+          const result = <p>{randomEntry.flavor_text}</p>;
+          console.log(result);
+          return result;}
+      catch(error){
+          const message = <p>"Oops! Something went wrong!"</p>
+          console.log(message);
+          return message;
+        };
+    }
+  }
+
+export default GetRandomEntry
